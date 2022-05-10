17e9e7dc9f3f07ffd04d756db6934a1112ce1cc6
Thiru22-dna <thirunavukkarasu16.samidurai@gmail.com>
2022-05-10 08:03:30 -0700

    First Commit

---
+++ eclipse-workspace/GitDemoProjectv2/src/demo.java
@@ -0,0 +1,19 @@
+
+public class demo {
+
+	public static void main(String[] args) {
+		
+		String hungry="yes";
+		
+		if(hungry=="yes") 
+			
+			System.out.println("eat somthong");
+		else {
+			
+			System.out.println("do work...");
+			
+		}
+
+	}
+
+}
