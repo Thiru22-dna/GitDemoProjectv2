480609eb18f429a72cb3e50161f8ccc5e3d45d9a
Thiru22-dna <100346296+Thiru22-dna@users.noreply.github.com>
2022-05-09 22:36:27 -0700

    Update Test.java

--- Eclipse-Project/src/Test.java
+++ Eclipse-Project/src/Test.java
@@ -1,9 +1,15 @@
 
 public class Test {
+	
+	int a=10;
+	int b=20;
+	int c;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Java");
+		c=a+b;
+		System.out.println(c);
 
 	}
 
